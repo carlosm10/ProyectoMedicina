cec1ebeb45314ab6c4a387df106375f4fead3d04
Carlos Mondragon <carlosmondragon10@gmail.com>
2015-09-25 11:18:05 -0500

    database connection

---
+++ Assets/SQLite4Unity3d/Scripts/Semana.cs
@@ -0,0 +1,13 @@
+﻿using SQLite4Unity3d;
+
+public class Semana  {
+	
+	[PrimaryKey, AutoIncrement]
+	public int Id { get; set; }
+	public string Nombre { get; set; }
+	
+	public override string ToString ()
+	{
+		return string.Format ("Pregunta: Id={0}, Nombre={1}", Id, Nombre);
+	}
+}
