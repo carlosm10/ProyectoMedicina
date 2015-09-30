8caaca19b131e8f131c5dbbb3cfc7ea5fbec5c8f
Priscila Sierra <priscila_sierra@alumno.um.edu.mx>
2015-09-30 10:36:50 -0500

    Menu interface, main and evaluation. Testing

---
+++ Assets/App/Scripts/LoadEvaluacion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadEvaluacion : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	void OnClick(){
+		Application.LoadLevel ("Evaluation");
+	}
+}
