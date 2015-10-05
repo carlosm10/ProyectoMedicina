9f1195c4db44bff32cd0840c4b4e8869d881b978
Carlos Mondragon <carlosmondragon10@gmail.com>
2015-10-05 08:56:07 -0500

    Sqlite4Unity3D added, integration fixed

--- Assets/App/Scripts/CheckBox.cs
+++ Assets/App/Scripts/CheckBox.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class CheckBox : MonoBehaviour{
 	//Base de Datos
-//	public DataService ds;
+	public DataService ds;
+
 	public GameObject parent;
 	public GameObject myCheckbox;
 	public UILabel pregunta;
@@ -13,14 +14,18 @@ public class CheckBox : MonoBehaviour{
 	UILabel label;
 	UIToggle toggle;
 	GameObject newCheck;
+
 	//Variable para label
 	string answer;
-	int idPregunta=1;
+	//Variable para numero de pregunta
+//	in  t idPregunta=1;
 
 	
 	void Start(){
 		pregunta.text = "Pregunta sobre semana "+Session.numSemana;
-//		getAnswers();
+
+		getAnswers();
+
 		createCheckbox ();
 		createCheckbox ();
 		createCheckbox ();
@@ -32,13 +37,14 @@ public class CheckBox : MonoBehaviour{
 	}
 
 	//Metodo para obtener las  Respuestas desde la base de datos.
-//	public void getAnswers(){
-//		ds = new DataService("medicina.db");	/*Especifia nombre de la base de datos*/
-//		var respuestas=ds.GetRespuestas(idPregunta); /*El numero 1 es el ID de la pregunta que estamos evaluando*/
-//		foreach(var respuesta in respuestas){
-//			answer = respuesta.Texto;
-//		}
-//	}
+	public void getAnswers(){
+		ds = new DataService("medicina.db");	/*Especifia nombre de la base de datos*/
+		ds.CreateDB ();
+		var respuestas=ds.GetRespuestas(); /*El numero 1 es el ID de la pregunta que estamos evaluando*/
+		foreach(var respuesta in respuestas){
+			answer = respuesta.Texto;
+		}
+	}
 	
 	public void createCheckbox(){
 		newCheck = (GameObject)Instantiate(myCheckbox);
@@ -53,8 +59,8 @@ public class CheckBox : MonoBehaviour{
 	public void setUp(){
 		label= newCheck.GetComponentInChildren<UILabel>();
 		toggle= newCheck.GetComponent<UIToggle>();
-//		label.text=answer;
-		label.text=text;
+		label.text=answer;
+//		label.text=text;
 		toggle.group = 1;
 		toggle.optionCanBeNone = true;
 		toggle.startsActive = false;
---
+++ Assets/SQLite4Unity3d/Scripts/CreateDBScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CreateDBScript : MonoBehaviour {
+
+	public Text DebugText;
+
+	// Use this for initialization
+	void Start () {
+		StartSync();
+	}
+
+    private void StartSync()
+    {
+        var ds = new DataService("medicina.db");
+        ds.CreateDB();
+        
+        var people = ds.GetPersons ();
+        ToConsole (people);
+//        people = ds.GetPersonsNamedRoberto ();
+//        ToConsole("Searching for Roberto ...");
+//        ToConsole (people); 
+
+		var preguntas = ds.GetPreguntas ();
+		ToConsole (preguntas);
+
+		var respuestas = ds.GetRespuestas ();
+		ToConsole (respuestas);
+
+    }
+
+	private void ToConsole(IEnumerable<Respuesta> respuestas){
+		foreach (var respuesta in respuestas) {
+			ToConsole(respuesta.ToString());
+		}
+	}
+
+	private void ToConsole(IEnumerable<Pregunta> preguntas){
+		foreach (var pregunta in preguntas) {
+			ToConsole(pregunta.ToString());
+		}
+	}
+
+	private void ToConsole(IEnumerable<Person> people){
+		foreach (var person in people) {
+			ToConsole(person.ToString());
+		}
+	}
+	
+	private void ToConsole(string msg){
+		DebugText.text += System.Environment.NewLine + msg;
+		Debug.Log (msg);
+	}
+}
